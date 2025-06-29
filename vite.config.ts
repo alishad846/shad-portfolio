3e7ffa2d8bbfa8a39b040f7f0ac1e756d3ee4fc4
Shad ali <133594315+alishad846@users.noreply.github.com>
2025-06-29 12:22:50 +0530

    Remove base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/shad-portfolio/', 
   plugins: [react()],
   optimizeDeps: {
     include: ['three', '@react-three/fiber', '@react-three/drei'],
